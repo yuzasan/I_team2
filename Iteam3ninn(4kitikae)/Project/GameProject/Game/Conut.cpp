e7938a9268fc04d0f5f9f29d6178099caf7579a9
yuza <hi1de2osan3@gmail.com>
2022-10-24 03:28:55 +0900

    kousinn

--- Iteam3ninn(4kitikae)/Project/GameProject/Game/Conut.cpp
+++
@@ -1,9 +0,0 @@
-#include "Conut.h"
-
-Conut::Conut():Base(eType_Conut) {
-	m_k = 1;
-}
-
-void Conut::Update(){
-	m_k = 1;
-}
--- Iteam3ninn(4kitikae)/Project/GameProject/Game/Conut.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Base/Base.h"
-
-class Conut : public Base {
-	CImage m_img;
-public:
-	Conut();
-	void Update();
-};
